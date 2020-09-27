07786e15c8114c49ef1070232d14b49547811845
fulstaph <bokovikov.g@wildberries.ru>
2020-09-27 16:07:06 +0300

    clippy unused import fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
